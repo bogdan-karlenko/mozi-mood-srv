ac9e1c939566a906164d7b44ff2fc9f015b0faa8
Alexander <hagriel@gmail.com>
2017-12-01 12:29:50 +0200

    socket message write to DB

--- src/server/app.js
+++ src/server/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const login = require('./routes/login');
+const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const socketio = require('socket.io');
 const http = require('http');
 const cors = require('cors');
 
+const login = require('./routes/login');
+
 const app = express();
-const server = http.createServer(app);
+//const server = http.createServer(app);
 
 const socketServer = http.createServer();
 const io = socketio(socketServer);
@@ -17,23 +19,48 @@ app.use(bodyParser.json())
 
 app.use('/login', login);
 
+const url = "mongodb://localhost:27017/mozi-mood-srv";
 const port = 8011;
 
-app.listen(port, () => {
-  console.log('----- \n Server is running on port', port, '\n-----');
-})
+writeToDB = (data, collection) => {
+  MongoClient.connect(url)
+    .then(db => {
+      db.collection(collection).insert(data);
+      db.close();
+    })
+    .catch(err => {
+      console.log(err);
+    })
+}
+
+clearCollectionDB = (collection) => {
+  MongoClient.connect(url)
+    .then(db => {
+      db.collection(collection).remove();
+      db.close();
+    })
+    .catch(err => {
+      console.log(err);
+    })
+}
 
 io.on('connection', (socket) => {
   console.log('a user connected', Object.keys(io.sockets.connected));
   socket.on('mood_event', (msg) => {
-    console.log(msg);
+    writeToDB({ mood: JSON.parse(msg) },
+      'mood');
+    console.log(JSON.parse(msg));
   })
+  socket.on('disconnect', (socket) => {
+    console.log('a user disconnected');
+  });
 });
-io.on('disconnection', (socket) => {
-  console.log('a user disconnected');
-});
-
 
 socketServer.listen(3000, () => {
   console.log('----- \n Socket Server is running on port', 3000, '\n-----');
+  clearCollectionDB('mood');
+})
+
+app.listen(port, () => {
+  console.log('----- \n Server is running on port', port, '\n-----');
 })
